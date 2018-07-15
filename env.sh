b0a2abb0040b5f5eeda6710b997d80730bf132b4
sunil.tailor <sunil.tailor@indexfeed.com>
2018-07-16 00:00:14 +0100

    added UrbanCode plugins

--- env.sh
+++ env.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-export UCD_SERVER_IP=$(docker inspect ucd \
-| jq '.[]|.NetworkSettings|.Networks|.bridge|.IPAddress' \
-| tr -d '"')
+# export UCD_SERVER_IP=$(docker inspect ucd \
+# | jq '.[]|.NetworkSettings|.Networks|.bridge|.IPAddress' \
+# | tr -d '"')
+
+docker inspect --format '{{ .NetworkSettings.IPAddress }}' ucd
